84fdb12e686fcf4b1393a21130e8cab5e693fbdf
Jimmy Envall <jimmyenvall@gmail.com>
2020-12-27 13:21:18 +0100

    Day 20 part 2

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::path::PathBuf;
 use std::rc::Rc;
 use std::cell::RefCell;
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashSet, HashMap, VecDeque, BinaryHeap};
 use std::hash::Hash;
 
 type Map = Vec<Vec<Tile>>;
@@ -46,6 +46,34 @@ fn build_map(input: &String) -> Map {
     }
 
     map
+}
+
+fn get_frame(map: &Map) -> HashSet<Vec2u> {
+    let mut queue: VecDeque<Vec2u> = vec![Vec2u{r: 0, c: 0}].into_iter().collect();
+    let mut visited = HashSet::new();
+
+    while !queue.is_empty() {
+        let current = queue.pop_front().unwrap();
+        
+        if visited.contains(&current) {
+            continue;
+        } else {
+            visited.insert(current.clone());
+        }
+
+        if let Some(tile) = get(map, current.r, current.c) {
+            match tile {
+                Tile::Empty | Tile::PortalHalf(_) => {
+                    for neighbour in current.get_neighbours().into_iter() {
+                        queue.push_back(neighbour);
+                    }
+                }
+                _ => ()
+            }
+        }
+    }
+
+    visited
 }
 
 fn get_portals(map: &Map) -> HashMap<String, Vec<Vec2u>> {
@@ -100,16 +128,40 @@ fn get(map: &Map, r: usize, c: usize) -> Option<Tile> {
     None
 }
 
-fn get_transfer_table(portals: &HashMap<String, Vec<Vec2u>>) -> HashMap<Vec2u,Vec2u> {
+#[derive(Debug)]
+struct Destination {
+    position: Vec2u,
+    level_difference: i32
+}
+
+impl Destination {
+    fn new(frame: &HashSet<Vec2u>, from_position: &Vec2u, to_position: Vec2u) -> Self {
+        let destination;
+
+        if frame.contains(from_position) {
+            destination = Destination{position: to_position, level_difference: -1};
+        } else {
+            destination = Destination{position: to_position, level_difference: 1};
+        }
+
+        destination
+    }
+}
+
+fn get_transfer_table(map: &Map, portals: &HashMap<String, Vec<Vec2u>>) -> HashMap<Vec2u,Destination> {
     let mut transfer_table = HashMap::new();
+    let frame = get_frame(map);
 
     for (_, positions) in portals.iter() {
         if positions.len() != 2 {
             continue;
         }
 
-        transfer_table.insert(positions.first().unwrap().clone(), positions.last().unwrap().clone());
-        transfer_table.insert(positions.last().unwrap().clone(), positions.first().unwrap().clone());
+        let first_position = positions.first().unwrap().clone();
+        let last_position = positions.last().unwrap().clone();
+
+        transfer_table.insert(first_position, Destination::new(&frame, &first_position, last_position.clone()));
+        transfer_table.insert(last_position, Destination::new(&frame, &last_position, first_position));
     }
 
     transfer_table
@@ -167,34 +219,34 @@ impl ExplorationState {
     }
 }
 
-fn part1(input: String) -> u32 {
+fn part1(input: &String) -> u32 {
     
-    let map = build_map(&input);
+    let map = build_map(input);
     let portals = get_portals(&map);
-    let transfer_table = get_transfer_table(&portals);
+    let transfer_table = get_transfer_table(&map, &portals);
     let start = *portals.get("AA").unwrap().first().unwrap(); 
     let goal = *portals.get("ZZ").unwrap().first().unwrap();
 
     let mut queue = vec![ExplorationState::new(start, 0)].into_iter().collect::<VecDeque<_>>();
-    let mut visited: HashMap<Vec2u, ExplorationState> = HashMap::new();
+    let mut visited: HashMap<Vec2u, u32> = HashMap::new();
 
     let mut min_distance = std::u32::MAX;
 
     while !queue.is_empty() {
-        let state = queue.pop_back().unwrap();
+        let state = queue.pop_front().unwrap();
 
         if state.current_position == goal {
             min_distance = std::cmp::min(min_distance, state.distance_travelled);
         }
 
-        if let Some(previously_visited_state) = visited.get_mut(&state.current_position) {
-            if state.distance_travelled < previously_visited_state.distance_travelled {
-                *previously_visited_state = state.clone();
+        if let Some(previous_distance) = visited.get_mut(&state.current_position) {
+            if state.distance_travelled < *previous_distance {
+                *previous_distance = state.distance_travelled;
             }else{
                 continue;
             }
         } else {
-            visited.insert(state.current_position, state.clone());
+            visited.insert(state.current_position, state.distance_travelled);
         }
 
         for neighbour_position in state.current_position.get_neighbours().into_iter() {
@@ -204,8 +256,8 @@ fn part1(input: String) -> u32 {
             }
         }
 
-        if let Some(teleport_position) = transfer_table.get(&state.current_position) {
-            let next_state = ExplorationState::new(*teleport_position, state.distance_travelled + 1);
+        if let Some(teleport_destination) = transfer_table.get(&state.current_position) {
+            let next_state = ExplorationState::new(teleport_destination.position, state.distance_travelled + 1);
             queue.push_back(next_state);
         }
     }
@@ -213,20 +265,121 @@ fn part1(input: String) -> u32 {
     min_distance
 }
 
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct LevelExplorationState {
+    current_position: Vec2u,
+    level: i32,
+    distance_travelled: u32
+}
+
+impl LevelExplorationState {
+    fn new(current_position: Vec2u, level: i32, distance_travelled: u32) -> LevelExplorationState {
+        LevelExplorationState{current_position, level, distance_travelled}
+    }
+}
+
+impl std::cmp::Ord for LevelExplorationState {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.level.cmp(&self.level)
+    }
+}
+
+impl PartialOrd for LevelExplorationState {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn part2(input: &String) -> u32 {
+
+    let map = build_map(input);
+    let portals = get_portals(&map);
+    let transfer_table = get_transfer_table(&map, &portals);
+    let start = *portals.get("AA").unwrap().first().unwrap(); 
+    let goal = *portals.get("ZZ").unwrap().first().unwrap();
+
+    let mut queue = vec![LevelExplorationState::new(start, 0, 0)].into_iter().collect::<BinaryHeap<_>>();
+    let mut visited: HashMap<(Vec2u, i32), u32> = HashMap::new();
+
+    let mut min_distance = std::u32::MAX;
+
+    while !queue.is_empty() {
+        let state = queue.pop().unwrap();
+
+        if state.level == 0 && state.current_position == goal {
+            min_distance = std::cmp::min(min_distance, state.distance_travelled);
+        }
+
+        if state.distance_travelled >= min_distance {
+            continue;
+        }
+
+        let key = (state.current_position, state.level);
+
+        if let Some(previous_distance) = visited.get_mut(&key) {
+            if state.distance_travelled < *previous_distance {
+                *previous_distance = state.distance_travelled;
+            }else{
+                continue;
+            }
+        } else {
+            visited.insert(key, state.distance_travelled);
+        }
+
+        for neighbour_position in state.current_position.get_neighbours().into_iter() {
+            if let Some(Tile::Path) = get(&map, neighbour_position.r, neighbour_position.c) {
+                let next_state = LevelExplorationState::new(neighbour_position, state.level, state.distance_travelled + 1);
+                queue.push(next_state);
+            }
+        }
+
+        if let Some(teleport_destination) = transfer_table.get(&state.current_position) {
+            let next_state = LevelExplorationState::new(teleport_destination.position, state.level + teleport_destination.level_difference, state.distance_travelled + 1);
+            if next_state.level >= 0 {
+                queue.push(next_state);
+            }
+        }
+    }
+
+    /*for (r, row) in map.iter().enumerate() {
+        let mut to_print = String::new();
+        for (c, col) in row.iter().enumerate() {
+            if let Some(destination) = transfer_table.get(&Vec2u{r, c}) {
+                match destination.level_difference {
+                    1 => to_print += "+",
+                    _ => to_print += "-"
+                }
+            }else{
+                match col {
+                    Tile::Empty => to_print += " ",
+                    Tile::Wall => to_print += "#",
+                    Tile::Path => to_print += ".",
+                    Tile::PortalHalf(c) => to_print += &c.to_string()
+                }
+            }
+        }
+        println!("{}", to_print);
+    }*/
+
+    min_distance
+}
+
 fn main() {
     let input = get_input();
 
-    let steps = part1(input.clone());
+    let steps = part1(&input);
     println!("part 1 steps: {}", steps);
+
+    let steps = part2(&input);
+    println!("part 2 steps: {}", steps);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn part1_test0() {
-        let input = r#"
+    fn get_input0() -> String {
+        r#"
          A           
          A           
   #######.#########  
@@ -245,17 +398,11 @@ DE..#######...###.#
 FG..#########.....#  
   ###########.#####  
              Z       
-             Z           
-            "#.to_owned();
-
-        let steps = part1(input);
-
-        assert_eq!(steps, 23);
+             Z     "#.to_owned()
     }
 
-    #[test]
-    fn part1_test1() {
-        let input = r#"
+    fn get_input1() -> String {
+                  r#"
                    A               
                    A               
   #################.#############  
@@ -292,11 +439,33 @@ YN......#               VT..#....QG
   #.#.........#...#.............#  
   #########.###.###.#############  
            B   J   C               
-           U   P   P                                        
-            "#.to_owned();
+           U   P   P             "#.to_owned()
+    }
 
-        let steps = part1(input);
+    #[test]
+    fn part1_test0() {
+        let input = get_input0();
+
+        let steps = part1(&input);
+
+        assert_eq!(steps, 23);
+    }
+
+    #[test]
+    fn part1_test1() {
+        let input = get_input1();
+
+        let steps = part1(&input);
 
         assert_eq!(steps, 58);
+    }
+
+    #[test]
+    fn part2_test0() {
+        let input = get_input0();
+
+        let steps = part2(&input);
+
+        assert_eq!(steps, 26);
     }
 }
